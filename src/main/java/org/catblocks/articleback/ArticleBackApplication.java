0bfd09b6f62ecf4144e2bd60cf960560b6e78613
Никита Николаевич Ростовцев <8200592@vc.miet.ru>
2022-10-06 13:32:02 +0300

    Initial commit

---
+++ src/main/java/org/catblocks/articleback/ArticleBackApplication.java
@@ -0,0 +1,13 @@
+package org.catblocks.articleback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/catblocks/articleback/ArticleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.catblocks.articleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
