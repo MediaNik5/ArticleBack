7b914e8851051e00655b5063f8ca1f5fc62e424c
MediaNik <stafilopok@mail.ru>
2022-10-20 13:45:21 +0300

    Add google auth

---
+++ src/main/java/org/catblocks/articleback/security/WebConfiguration.java
@@ -0,0 +1,25 @@
+package org.catblocks.articleback.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+public class WebConfiguration  extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/", "/error", "/oauth2/authorization/google").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                .oauth2Login();
+    }
+}
