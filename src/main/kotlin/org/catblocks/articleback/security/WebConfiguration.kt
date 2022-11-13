8a577a262480bb1668804666b1d9eab22658e830
MediaNik5 <stafilopok@mail.ru>
2022-11-13 19:59:40 +0300

    Fix github actions ci

--- src/main/kotlin/org/catblocks/articleback/security/WebConfiguration.kt
+++ src/main/kotlin/org/catblocks/articleback/security/WebConfiguration.kt
@@ -1,100 +1,75 @@
-package org.catblocks.articleback.security;
+package org.catblocks.articleback.security
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.catblocks.articleback.security.token.AccessTokenResponseClient;
-import org.catblocks.articleback.security.token.HttpCookieOAuth2AuthorizationRequestRepository;
-import org.catblocks.articleback.security.token.OAuth2AuthenticationFailureHandler;
-import org.catblocks.articleback.security.token.OAuth2AuthenticationSuccessHandler;
-import org.catblocks.articleback.security.token.TokenAuthenticationFilter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import org.catblocks.articleback.security.token.*
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-@SecurityScheme(
-        name = "Bearer Authentication",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
+@SecurityScheme(name = "Bearer Authentication", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 @EnableWebSecurity
-public class WebConfiguration  extends WebSecurityConfigurerAdapter {
-    private final HttpCookieOAuth2AuthorizationRequestRepository cookieRepository;
-    private final AccessTokenResponseClient accessTokenResponseClient;
-    private final CustomOAuth2UserService oAuth2UserService;
-    private final OAuth2AuthenticationSuccessHandler successHandler;
-    private final OAuth2AuthenticationFailureHandler failureHandler;
-    private final TokenAuthenticationFilter tokenAuthenticationFilter;
-
-    public WebConfiguration(
-        HttpCookieOAuth2AuthorizationRequestRepository cookieRepository,
-        AccessTokenResponseClient accessTokenResponseClient,
-        CustomOAuth2UserService oAuth2UserService,
-        OAuth2AuthenticationSuccessHandler successHandler,
-        OAuth2AuthenticationFailureHandler failureHandler,
-        TokenAuthenticationFilter tokenAuthenticationFilter
-    ){
-        this.cookieRepository = cookieRepository;
-        this.accessTokenResponseClient = accessTokenResponseClient;
-        this.oAuth2UserService = oAuth2UserService;
-        this.successHandler = successHandler;
-        this.failureHandler = failureHandler;
-        this.tokenAuthenticationFilter = tokenAuthenticationFilter;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+class WebConfiguration(
+    private val cookieRepository: HttpCookieOAuth2AuthorizationRequestRepository,
+    private val accessTokenResponseClient: AccessTokenResponseClient,
+    private val oAuth2UserService: CustomOAuth2UserService,
+    private val successHandler: OAuth2AuthenticationSuccessHandler,
+    private val failureHandler: OAuth2AuthenticationFailureHandler,
+    private val tokenAuthenticationFilter: TokenAuthenticationFilter,
+) : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
         http
             .cors()
-                .and()
+            .and()
             .csrf()
-                .disable()
+            .disable()
             .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .authorizeRequests()
-                .antMatchers(
-                    "/",
-                    "/error",
-                    "/oauth/**",
-                    "/oauth2/**",
-                    "/users/{id}",
-                    "/swagger-ui/index.html",
-                    "/swagger-ui/**",
-                    "/v3/api-docs/**"
-                ).permitAll()
-                .antMatchers(HttpMethod.GET, "/articles/{id}")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
+            .antMatchers(
+                "/",
+                "/error",
+                "/oauth/**",
+                "/oauth2/**",
+                "/users/{id}",
+                "/swagger-ui/index.html",
+                "/swagger-ui/**",
+                "/v3/api-docs/**"
+            ).permitAll()
+            .antMatchers(HttpMethod.GET, "/articles/{id}")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
             .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                .and()
+            .authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            .and()
             .oauth2Login()
-                .authorizationEndpoint()
-                    .baseUri("/oauth2/authorize")
-                    .authorizationRequestRepository(cookieRepository)
-                    .and()
-                .redirectionEndpoint()
-                    .baseUri("/oauth2/callback/*")
-                    .and()
-                .userInfoEndpoint()
-                    .userService(oAuth2UserService)
-                    .and()
-                .tokenEndpoint()
-                    .accessTokenResponseClient(accessTokenResponseClient)
-                    .and()
-                .successHandler(successHandler)
-                .failureHandler(failureHandler)
-                .and()
-            .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+            .authorizationEndpoint()
+            .baseUri("/oauth2/authorize")
+            .authorizationRequestRepository(cookieRepository)
+            .and()
+            .redirectionEndpoint()
+            .baseUri("/oauth2/callback/*")
+            .and()
+            .userInfoEndpoint()
+            .userService(oAuth2UserService)
+            .and()
+            .tokenEndpoint()
+            .accessTokenResponseClient(accessTokenResponseClient)
+            .and()
+            .successHandler(successHandler)
+            .failureHandler(failureHandler)
+            .and()
+            .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
     }
 }
